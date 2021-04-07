4759490bbc21afcc3e6a0303ad6d03eb0fb6cd9a
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-04-07 16:37:16 +0530

    Post and get Express node Done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('stuff'));
@@ -8,9 +11,14 @@ app.get('/', function (request, response) {
     response.render('index')
 });
 
+app.post('/contact', urlencodedParser, function (request, response) {
+    console.log(request.body.email)
+    response.render('contactsuccess', { data: request.body })
+});
+
 app.get('/contact', function (request, response) {
     console.log(request.query)
-    response.render('contact', {qs: request.query})
+    response.render('contact', { qs: request.query })
 });
 
 app.get('/profile/:name', function (request, response) {
